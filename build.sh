c3b4319b26383cb96b57e24b3d76adf6c7f777e3
Roberto Sassu <roberto.sassu@huawei.com>
2021-04-07 17:36:10 +0200

    Add evm_hash_algo_hmac.test

--- build.sh
+++ build.sh
@@ -85,6 +85,7 @@ if [ $ret -eq 0 ]; then
 	tail -100 tests/portable_signatures.log
 	tail -100 tests/ima_skip_measurement_audit.log
 	tail -100 tests/ima_meta_immutable.log
+	tail -100 tests/evm_hash_algo_hmac.log
 	exit 0
 fi
 
